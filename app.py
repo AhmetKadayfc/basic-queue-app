f7bedceec1914de725b4a66a1a0837678ca9f8a0
Ahmet Kadayıfçı <ahmetkadayfc@hotmail.com>
2022-04-24 16:44:57 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,72 @@
+import time
+from typing import Callable
+
+from threading import Thread
+from queue import Queue
+
+from tasks import task_1, task_2, task_3
+
+blocking = False
+
+def queue_controller(work_queue: Queue):
+    if work_queue.empty():
+        print("Queue nothing to do")
+    else:
+        print("Queue running")
+        print('----------------------')
+        while not work_queue.empty():
+            print(f'Queue size: {work_queue.qsize()}')
+            task = work_queue.get()
+
+            task_thread = Thread(target=task, daemon=True)
+            task_thread.start()
+            task_thread.join()
+            work_queue.task_done()
+        print('----------------------')
+
+
+def blocker_task_controller(func: Callable[..., None], work_queue: Queue):
+    global blocking
+
+    if blocking:
+        work_queue.put(func)
+        print('task added to queue')
+    else:
+        work_thread = Thread(target=func, daemon=True)
+        work_thread.start()
+        work_thread.join()
+
+
+def blocker_task(queue: Queue):
+    global blocking
+
+    print('Blocking')
+    blocking = True
+    time.sleep(2)
+    blocking = False
+    print('Blocking done!')
+    if not queue.empty():
+        Thread(target=queue_controller, args=(queue,)).start()
+        queue.join()
+        print('Tasks done!')
+
+
+def main():
+    """
+    This is the main entry point for the program
+    """
+    # Create the queue of work
+    worker_queue = Queue()
+
+    blocker_task_controller(task_1, worker_queue)
+
+    Thread(target=blocker_task, args=(worker_queue,)).start()
+    time.sleep(0.3)
+    
+    # Put some work in the queue
+    blocker_task_controller(task_2, worker_queue)
+    blocker_task_controller(task_3, worker_queue)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tasks.py
@@ -0,0 +1,17 @@
+import time
+
+
+def task_1():
+    print('Task 1 running')
+    time.sleep(4)
+    print('Task 1 accomplished!')
+
+def task_2():
+    print('Task 2 running')
+    time.sleep(3)
+    print('Task 2 accomplished!')
+
+def task_3():
+    print('Task 3 running')
+    time.sleep(1)
+    print('Task 3 accomplished!')
