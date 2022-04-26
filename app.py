18ec2af1d51ae6b19f0bb6558c5015d3fee7342b
Ahmet Kadayıfçı <ahmetkadayfc@hotmail.com>
2022-04-26 13:05:05 +0300

    tasks made dynamic

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import asyncio
 import time
+
 from typing import Callable
 
 from threading import Thread
 from queue import Queue
 
-from tasks import task_1, task_2, task_3
+from tasks import task_1, task_2, task_3, task_4
 
 blocking = False
 
@@ -17,24 +19,30 @@ def queue_controller(work_queue: Queue):
         while not work_queue.empty():
             print(f'Queue size: {work_queue.qsize()}')
             task = work_queue.get()
-
-            task_thread = Thread(target=task['func'], args=(*task['args'],), daemon=True)
-            task_thread.start()
-            task_thread.join()
+            task_runner(task)
             work_queue.task_done()
         print('----------------------')
+        work_queue.join()
+        print('Tasks done!')
 
 
-def blocker_task_controller(func: Callable[..., None], work_queue: Queue, *args):
+def blocker_task_controller(func: Callable[..., None], work_queue: Queue, func_type: str, *args):
     global blocking
-    task = {'func': func, 'args': args}
+    task = {'func': func, 'args': args, 'func_type': func_type} 
     if blocking:
         work_queue.put(task)
         print('task added to queue')
     else:
-        work_thread = Thread(target=func, args=(*args,), daemon=True)
-        work_thread.start()
-        work_thread.join()
+        task_runner(task)
+
+
+def task_runner(task: dict):
+    if task['func_type'] == 'async':
+        task_thread = Thread(target=asyncio.run, args=(task['func'](*task['args']),), daemon=True)
+    else:
+        task_thread = Thread(target=task['func'], args=(*task['args'],), daemon=True)
+    task_thread.start()
+    task_thread.join()
 
 
 def blocker_task(queue: Queue):
@@ -47,8 +55,6 @@ def blocker_task(queue: Queue):
     print('Blocking done!')
     if not queue.empty():
         Thread(target=queue_controller, args=(queue,)).start()
-        queue.join()
-        print('Tasks done!')
 
 
 def main():
@@ -57,15 +63,15 @@ def main():
     """
     # Create the queue of work
     worker_queue = Queue()
-
-    blocker_task_controller(task_1, worker_queue, 23)
+    blocker_task_controller(task_1, worker_queue, 'async', 23)
+    blocker_task_controller(task_4, worker_queue, 'sync')
 
     Thread(target=blocker_task, args=(worker_queue,)).start()
     time.sleep(0.3)
     
     # Put some work in the queue
-    blocker_task_controller(task_2, worker_queue, 12, 21)
-    blocker_task_controller(task_3, worker_queue)
+    blocker_task_controller(task_2, worker_queue, 'async', 12, 21)
+    blocker_task_controller(task_3, worker_queue, 'sync')
 
 
 if __name__ == "__main__":
--- tasks.py
+++ tasks.py
@@ -1,19 +1,28 @@
+import asyncio
 import time
 
 
-def task_1(num_1):
+async def task_1(num_1):
     print('Task 1 running')
     print(f'Number is {num_1}')
-    time.sleep(4)
+    await asyncio.sleep(3)
     print('Task 1 accomplished!')
 
-def task_2(num_1, num_2):
+
+async def task_2(num_1, num_2):
     print('Task 2 running')
     print(f'Sum: {num_1+num_2}')
-    time.sleep(3)
+    await asyncio.sleep(2)
     print('Task 2 accomplished!')
+
 
 def task_3():
     print('Task 3 running')
     time.sleep(1)
     print('Task 3 accomplished!')
+
+
+def task_4():
+    print('Task 4 running')
+    time.sleep(1)
+    print('Task 4 accomplished!')
